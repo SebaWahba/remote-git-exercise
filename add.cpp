a362bd7f4e86c2e8231cd785f95ad23036610f87
sebawahba <sebawahba@aucegypt.edu>
2024-02-06 11:20:02 +0200

    Additional part to file.cpp

--- add.cpp
+++ add.cpp
@@ -4,3 +4,12 @@
 {\*\expandedcolortbl;;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh15060\viewkind0
 }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin>>x>>y;
+    cout<<x+y;
+    return 0;
+}
